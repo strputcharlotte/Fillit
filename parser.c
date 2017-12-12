92347d5e8816f5c93a0b85c8312a2397de1078ba
Charlotte FARJANEL <cfarjane@e3r3p1.42.fr>
2017-12-12 18:00:42 +0100

    truc

--- parser.c
+++ parser.c
@@ -6,11 +6,64 @@
 /*   By: cfarjane <cfarjane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/09 13:12:33 by cfarjane          #+#    #+#             */
-/*   Updated: 2017/12/10 15:28:30 by emoreau          ###   ########.fr       */
+/*   Updated: 2017/12/12 17:58:46 by cfarjane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "includes.h"
 
-// probablement que cette fonction te servait a faire des tests mais
-//elle me rajoutait des erreurs dans la compil
+/*
+ * 4 # mais pas collés;
+ * mauvais nombre de points;
+ * */
+
+int		error(char *line, int fd, int nbtetro)
+{
+	int		i;
+	int		cpt;
+	int		cpt_pts;
+	char	buff_fixe[5];
+	char	*buff_var;
+
+	//if (!(buff_var = malloc(sizeof(char) * 5)))
+	//return (0);
+	if (line[4] != '\n')
+		error_exit("Not a valid tetro", 100);
+	if (nbtetro > 26 || nbtetro < 1)
+		error_exit("Too many or too few tetros", 105);
+	cpt = 0;
+	i = 0;
+	while (line[i] == '#')
+	{
+		i++;
+		cpt++;
+		if (*cpt > 4)
+			error_exit("Not a valid tetro", 102);
+		if (line[i] == '\0' && *cpt != 4)
+			error_exit("Not a valid tetro", 103);
+	}
+	while (line[i] < 4)
+	{
+		if ((line[i] != '.' || line[i] != '#') && line[i] < 3)
+			error_exit("Not a valid tetro", 101);
+		i++;
+	}
+	return (fd);
+}
+
+int		**parser(int **tetro, char *line)
+{
+	int i;
+	int cpt; //compteur \n
+
+	//malloc tetro
+	i = 0;
+	//if 5 \n -> new tetro
+	//if line[4] = \n -> recommence
+	while (cpt < 4)
+	{
+		if (line[i] == '\n')
+			cpt++;
+		i++;
+	}
+}
--- read.c
+++ read.c
@@ -6,7 +6,7 @@
 /*   By: cfarjane <cfarjane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 15:53:51 by cfarjane          #+#    #+#             */
-/*   Updated: 2017/12/10 17:20:08 by emoreau          ###   ########.fr       */
+/*   Updated: 2017/12/12 18:00:15 by cfarjane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,10 @@
 #include "error_exit.c"
 #include "includes.h"
 
-/* Open est une fonction qui existe deja et qu'on a pas besoin de recoder
-Pareil pour read.
-
-int		open(char *fd, int O_RDONLY)
-{
-	fd = "parser.c";
-	if (fd)
-		return (1);
-	else
-		return (-1);
-}
-
-*/
-
-char		*ft_read(char *argv)
+char	*ft_read(char *argv)
 {
 	int fd;
-//buff_fixe[5] => 5 char par ligne (4 #/. ; 1 \n)
-/*
-	char buff_fixe[5];
-	char *buff_var;
-*/
+
 	if ((fd = open(argv, O_RDONLY)) == -1)
 		error_exit("File doesn't exist", 100);
 	int ret;
@@ -45,6 +27,4 @@ char		*ft_read(char *argv)
 	ret = read(fd, str, BUFF_SIZE);
 	str[ret] = 0;
 	return (ft_strdup(str));
-//	if (!(buff_var = malloc(sizeof(char) * 5)))
-//		return (0);
 }
