4df37d227f9bc242a1069f03156973bdf0bea0d9
Esther MOREAU <emoreau@e3r2p2.42.fr>
2017-12-12 17:05:39 +0100

    ajout include

--- includes.h
+++ includes.h
@@ -6,7 +6,7 @@
 /*   By: cfarjane <cfarjane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 17:43:47 by cfarjane          #+#    #+#             */
-/*   Updated: 2017/12/10 16:16:53 by emoreau          ###   ########.fr       */
+/*   Updated: 2017/12/12 16:43:56 by emoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ char	*ft_read(char *argv);
 char	*ft_convletter(char *str);
 int		ft_compttetri(char *str);
 int		ft_sizemin(int nbrtetri);
+void	ft_emptymap(int size,char **map);
 
 #endif
