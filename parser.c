36cc2ec2168d27a4fb998ac7282f05575329a1e0
Charlotte FARJANEL <cfarjane@e2r10p5.42.fr>
2017-12-14 17:46:33 +0100

    avancee parser

--- parser.c
+++ parser.c
@@ -6,17 +6,18 @@
 /*   By: cfarjane <cfarjane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/09 13:12:33 by cfarjane          #+#    #+#             */
-/*   Updated: 2017/12/14 11:52:42 by cfarjane         ###   ########.fr       */
+/*   Updated: 2017/12/14 17:45:14 by cfarjane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.h"
 
-char	**parsing_tab(char **tab)
+//parse le tetro et verifie la taille du tab + le nbr de points
+char			**parsing_tab(char **tab)
 {
 	int x;
 	int y;
-	int cpt_pts;
+//	int cpt_pts;
 
 	x = 0;
 	y = 0;
@@ -25,14 +26,15 @@ char	**parsing_tab(char **tab)
 		x++;
 		y++;
 	}
-	if (!(*tab[x] == 3 && *tab[y] == 3))
-		error_exit("Wrong size of tab", 104);
-	if (cpt_pts != 12)
-		error_exit("Not a valid tetro", 106);
+	if (!(*tab[x] == 4 && *tab[y] == 4))
+		error_exit("Wrong size of tab", 100);
+//	if (cpt_pts != 12)
+//		error_exit("Not a valid tetro", 101);
 	return (tab);
 }
 
-int		error(char *line, int fd, int nbtetro)
+//parse chaque ligne, verifie le nombre de \n, 
+int				error(char *line, int fd, int nbtetro)
 {
 	int		i;
 	int		cpt;
@@ -43,48 +45,44 @@ int		error(char *line, int fd, int nbtetro)
 	if (!(buff_var = malloc(sizeof(char) * 5)))
 		return (0);
 	if (line[4] != '\n')
-		error_exit("Not a valid tetro", 100);
+		error_exit("Not a valid tetro", 102);
 	if (nbtetro > 26 || nbtetro < 1)
-		error_exit("Too many or too few tetros", 105);
+		error_exit("Too many or too few tetros", 103);
 	cpt = 0;
 	i = 0;
 	while (line[i] == '#' && (i++) && (cpt++))
 	{
 		if (cpt > 4)
-			error_exit("Not a valid tetro", 102);
+			error_exit("Not a valid tetro", 104);
 		if (line[i] == '\0' && cpt != 4)
-			error_exit("Not a valid tetro", 103);
+			error_exit("Not a valid tetro", 105);
 	}
 	while (line[i] < 4)
 		if ((line[i] != '.' || line[i] != '#') && line[i] < 3 && (i++))
-			error_exit("Not a valid tetro", 101);
+			error_exit("Not a valid tetro", 106);
 	return (fd);
 }
 
-char	*parser(char *tetro_line)
+//va a la ligne
+static char		*end(char *tetro_line)
 {
 	while (*tetro_line)
 		tetro_line++;
 	return (tetro_line);
 }
 
-char	**ft_parsing(char **tetro, char *line)
+char			**ft_parsing(char **tetro, char *line)
 {
 	int i;
 	int x;
 	int y;
 	int cpt_n;
 
-/*	if (!(tetro = (char**)malloc(sizeof(char*) * ((ft_strlen(line) * 4	) + 1))))
-	return (NULL);
-	while (**tab)
-	{
-		if (!(tetro[y] = (char*)malloc(sizeof(char) * (ft_strlen(line) + 1))))
-			return (NULL);
-	}*/
+	if (!(tetro = (char**)malloc(sizeof(char*) * (ft_strlen(line) * 4))))
+		return (NULL);
 	i = 0;
 	if (line[4] == '\n')
-		parser(line);
+		end(line);
 	while (cpt_n < 4)
 	{
 		if (line[i] == '\n')
@@ -92,14 +90,36 @@ char	**ft_parsing(char **tetro, char *line)
 		i++;
 	}
 	if (cpt_n == 4 && line[i] == '\n')
-		parser(line);
+		end(line);
 	return (tetro);
 }
 
-char	**checking(char **tab)
+//verifie si 4 # colles
+char			**checking(char **tab)
 {
 	int x;
 	int	y;
+	int	cpt;
 
+	x = 0;
+	y = 0;
+	cpt = 0;
+	while (**tab)
+	{
+		while (tab[y] && (y++))
+		{
+			if (y == '#')
+				cpt++;
+		}
+		while (tab[x] && (x++))
+		{
+			if (x == '#')
+				cpt++;
+		}
+	}
+	if (cpt == 6 && cpt == 8)
+		return (tab);
+	else
+		error_exit("Not a valid tetro", 107);
 	return (tab);
 }
