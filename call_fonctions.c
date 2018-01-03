7385c58f9439af513eaa243a7e17c28475dfc697
Charlotte FARJANEL <cfarjane@e2r4p10.42.fr>
2018-01-03 19:19:00 +0100

    avancee call_fonctions

--- call_fonctions.c
+++ call_fonctions.c
@@ -6,33 +6,52 @@
 /*   By: cfarjane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/29 15:23:38 by cfarjane          #+#    #+#             */
-/*   Updated: 2018/01/02 19:31:24 by cfarjane         ###   ########.fr       */
+/*   Updated: 2018/01/03 19:18:19 by cfarjane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.h"
+#include <stdio.h>
 
-//reduction tetro
-
-char	**plcmt(char **tab)
+char			**plcmt(char **tab)
 {
 	int x;
 	int y;
+	char **new;
 
+	x = 4;
+	y = 4;
+	if (!(new = (char**)malloc(sizeof(char*) * (x * y))))
+		return (NULL);
+//	if (!(new[x] = (char*)malloc(sizeof(char) * (ft_strlen(tab[x])))))
+//		return (NULL);
 	x = 0;
 	y = 0;
 	while (tab)
 	{
-		if (y == 4)
+		if (ft_strcmp(tab[y], "#") == 0)
 		{
-			x++;
-			y = 0;
+			y++;
+			if (y == 3)
+			{
+				x++;
+				y = 0;
+			}
 		}
-		y++;
+		if (ft_strcmp(tab[y], "#") != 0 && y <= 3)
+		{
+			new = new + y;
+			y++;
+		}
+		x++;
+		y = 0;
 	}
+	free(new);
+	printf("%s\n", *new);
+	return (new);
 }
 
-int		call_fonctions(int argc, char **argv)
+/*int			call_fonctions(int argc, char **argv)
 {
 	if (argc > 2)
 		error_exit("Too many arguments", 300);
@@ -43,4 +62,4 @@ int		call_fonctions(int argc, char **argv)
 		error(argv);
 	}
 	return (0);
-}
+}*/
