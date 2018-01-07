940a9153a5751590ffa124506c1a3a965d3183be
Esther MOREAU <emoreau@e3r11p10.42.fr>
2018-01-07 15:48:42 +0100

    modifs fonctions solver

--- includes.h
+++ includes.h
@@ -6,7 +6,7 @@
 /*   By: cfarjane <cfarjane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/08 17:43:47 by cfarjane          #+#    #+#             */
-/*   Updated: 2018/01/06 20:00:02 by cfarjane         ###   ########.fr       */
+/*   Updated: 2018/01/07 15:26:33 by emoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ int					ft_sizemin(int nbrtetri);
 int					ft_compttetri(char *str);
 int					error(char **tab);
 int					main(int argc, char **argv);
+char				**ft_define_tetri(char **tab, char c);
 
 #endif
--- solver_2.c
+++ solver_2.c
@@ -6,27 +6,59 @@
 /*   By: emoreau <emoreau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/02 17:29:34 by emoreau           #+#    #+#             */
-/*   Updated: 2018/01/07 13:31:22 by emoreau          ###   ########.fr       */
+/*   Updated: 2018/01/07 15:28:37 by emoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes.h"
 
-char	**ft_define_tetri(char **fichier_alltetri, char c)
+void	ft_free_tab()
+
+static int	ft_comptl(char **tab, char c)
 {
-	char **tetri;
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
-	ft_convletter(call_fonctions(**fichier_alltetri));
-	while (fichier_alltetri[i])
+	while (tab[j])
 	{
-		if (fichier_alltetri[i] == c)
-			tetri[j] = fichier_alltetri[i];
+		if (tab[j][i] == c)
+		{
+			i = -1;
+			j++;
+		}
 		i++;
-		j++;
+		if (tab[j][i] == c + 1)
+			return (j);
 	}
-	return (tetri);
+	return (0);
+}
+
+char	**ft_define_tetri(char **tab, char c)
+{
+	int i;
+	int j;
+	char **newline;
+	int y;
+
+	i = 0;
+	j = 0;
+	y = 0;
+	if (!(newline = (char**)malloc(sizeof(char *) * ft_comptl(tab, c) + 1)))
+		return (NULL);
+	while (tab[j])
+	{
+		if (tab[j][i] == c)
+		{
+			newline[y] = ft_strdup(tab[j]);
+			i = -1;
+			j++;
+			y++;
+		}
+		i++;
+		if (tab[j][i] == c + 1)
+			return (tab);
+	}
+	return (NULL);
 }
